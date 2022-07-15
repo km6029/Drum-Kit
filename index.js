29dee7de083f8ba2161d4236d145030ce3f762de
km6029 <91561312+km6029@users.noreply.github.com>
2022-07-15 21:55:54 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+document.querySelector(".set").addEventListener("click",function(){
+  var audio=new Audio("sounds/snare.mp3");
+  audio.play();
+});
+/*document.querySelector(".set").addEventListener("click",handleClick);
+function handleClick()
+{
+  alert("I got Clicked!");
+}*/
