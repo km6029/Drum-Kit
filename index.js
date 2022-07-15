62f926de9df6854ed59f32c196a61a05722223ee
km6029 <91561312+km6029@users.noreply.github.com>
2022-07-15 21:56:55 +0530

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-document.querySelector(".set").addEventListener("click",function(){
-  var audio=new Audio("sounds/snare.mp3");
-  audio.play();
-});
-/*document.querySelector(".set").addEventListener("click",handleClick);
-function handleClick()
-{
-  alert("I got Clicked!");
-}*/
